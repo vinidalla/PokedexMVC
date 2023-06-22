7102fd36bf344ed9468d1e7993f3cc7b5ba85a5c
vinicius.vechia <vinicius.vechia@e-unicred.com.br>
2023-06-22 10:33:39 -0300

    collectionView created and extensions for constraints

--- PokedexMVC/Controller/PokedexViewController.swift
+++ PokedexMVC/Controller/PokedexViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+private let reuseIdentifier: String = "PokedexCell"
+
 class PokedexViewController: UICollectionViewController {
   
   // MARK: - Properties
@@ -43,6 +45,30 @@ class PokedexViewController: UICollectionViewController {
                                                         target: self,
                                                         action: #selector(showSearchBar))
     navigationItem.rightBarButtonItem?.tintColor = .white
+    
+    collectionView.register(PokedexCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+  }
+}
+
+extension PokedexViewController {
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 6
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PokedexCell
+    return cell
+  }
+}
+
+extension PokedexViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    return UIEdgeInsets(top: 32, left: 8, bottom: 8, right: 8)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let width = (view.frame.width - 36)/3
+    return CGSize(width: width, height: width)
   }
 
 }
--- PokedexMVC/Utils/Extensions.swift
+++ PokedexMVC/Utils/Extensions.swift
@@ -14,7 +14,51 @@ extension UIColor {
   }
   
   static func mainColor() -> UIColor {
-    return UIColor.rgb(red: 5, green: 5, blue: 5)
+    return UIColor.rgb(red: 230, green: 30, blue: 30)
   }
 }
 
+extension UIView {
+  func center(inView view: UIView) {
+    self.translatesAutoresizingMaskIntoConstraints = false
+    self.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    self.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+  }
+  
+  func anchor(top: NSLayoutYAxisAnchor?,
+              bottom: NSLayoutYAxisAnchor?,
+              left: NSLayoutXAxisAnchor?,
+              right: NSLayoutXAxisAnchor?,
+              paddingTop: CGFloat,
+              paddingBottom: CGFloat,
+              paddingLeft: CGFloat,
+              paddingRight: CGFloat,
+              width: CGFloat,
+              height: CGFloat) {
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    if let top = top {
+      self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+    }
+    
+    if let bottom = bottom {
+      bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+    }
+    
+    if let left = left {
+      self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+    }
+    
+    if let right = right {
+      rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+    }
+    
+    if width != 0 {
+      widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+    
+    if height != 0 {
+      heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+  }
+}
